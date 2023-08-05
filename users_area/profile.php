888facf20c81a0c7887e56d25972aeb6915fe933
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-05 23:05:39 +0100

    made a reference

--- users_area/profile.php
+++ users_area/profile.php
@@ -68,7 +68,7 @@ session_start();
               <a class="nav-link" href="../cart.php"><i class="fa-solid fa-shopping-cart" ></i><sup><?php cart_item()?></sup></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Total Price: N<?php total_cart_price(); ?> </a>
+              <a class="nav-link" href="">Total Price: N<?php total_cart_price(); ?> </a>
             </li>
             </ul>
 
