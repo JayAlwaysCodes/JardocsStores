fd881ce39d58eb9ad527c996b73be1863698c076
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-07 04:25:30 +0100

    worked on confirming before deleting

--- admin_area/view_brands.php
+++ admin_area/view_brands.php
@@ -23,10 +23,28 @@
             <td><?php echo $number ?></td>
             <td><?php echo $brand_title ?></td>
             <td><a href='index.php?edit_brand=<?php echo $brand_id ?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i></a></td>
-            <td><a href='index.php?delete_brand=<?php echo $brand_id ?>' class='text-light'><i class='fa-solid fa-trash'></i></a></td>
+            <td><a href='index.php?delete_brand=<?php echo $brand_id ?>' class='text-light' type="button"data-toggle="modal" data-target="#exampleModal"><i class='fa-solid fa-trash'></i></a></td>
         
         </tr>
         <?php
         }?>
     </tbody>
 </table>
+<!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+    
+      <div class="modal-body">
+        <h5>Are you sure you want to delete this?</h5>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal"><a href="index.php?view_brands" class="text-light text-decoration-none">No</a></button>
+        <button type="button" class="btn btn-primary"><a href='index.php?delete_brand=<?php echo $brand_id ?>' class='text-light text-decoration-none' >Yes</a></button>
+      </div>
+    </div>
+  </div>
+</div>
