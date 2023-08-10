c1cc8d427f4688e17738f8a3d78a115d64667ac1
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-10 15:45:14 +0100

    edited the connect.php file after hosting the site

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '', 'jardocsstore');
+$con = mysqli_connect('sql109.ezyro.com', 'ezyro_34791701', 'Jardocs100%', 'ezyro_34791701_jardocsestore');
 if(!$con){
     die(mysqli_error($con));
 }
